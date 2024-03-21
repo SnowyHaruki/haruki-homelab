ad75f7a56023e50fa2c406e4cb6132ab07861097
Haruki <160177732+SnowyHaruki@users.noreply.github.com>
2024-03-21 19:15:11 +0000

    Update vars.tf
    + Add VMID and Hostname list variable

--- 0_automation_scritps/Terraform/vars.tf
+++ 0_automation_scritps/Terraform/vars.tf
@@ -8,3 +8,14 @@ variable "template_name" {
 variable "ssh_key" {
   default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPXNpIsGHVA6KAAe8wM9r8sPURxPl/mXOFgY5PLrR7jO debian@mistysnowrealms.net"
 }
+
+variable "vm_id" {
+  type = list(string)
+  default = ["100","101", "102"]
+}
+
+variable "vm_hostname" {
+  type = list(string)
+  default = ["msrl-home-00v","msrl-home-01v", "msrl-home-02v"]
+}
+   
