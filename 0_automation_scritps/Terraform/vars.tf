82a4730f11172128ef99eb375a1744cad91af2c7
SnowyHaruki <snowyharuki@gmail.com>
2024-03-31 21:57:12 +0100

    Terraform changes
    + Terraform version update
    + SSH key sensitive var

--- 0_automation_scritps/Terraform/vars.tf
+++ 0_automation_scritps/Terraform/vars.tf
@@ -7,6 +7,7 @@ variable "template_name" {
 
 variable "ssh_key" {
   default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCCFyBRbYik4yd9CzVmY0PJSKXukXwYPBkARyN3TipRjJffaF0Nfqsfl4nPQ6D3ElhoheDB8ecHjDGIZIQklmFGGKCOQqyiFoSog1Tc/FUhEvdwfW5toVEJ5alfSl3wZy7TSkBaRxqdRwfwS+lmkzCeHCQFeQL2KwPGO0b1ynXAakHg1N70XqhPztnYbz/7QqqtKuwM7GaP6Sy+6EYF4ABvjv4lQnby9/3kMuAXTNpM5GXLNhItlm9k0jtB0JQiuIeSIPZ0GUja1+EUzUgP/I1mW1oEvAiGynliH9Nw4LDjEFOmip8vosiBWO6d97P+38+f9COgok91RSJTSjom7AId misty@mistysnowrealms.net"
+  sensitive = true
 }
 
 variable "vm_id_home" {
